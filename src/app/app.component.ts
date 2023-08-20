2b6027b41774e449f2304b5e629f2ff4ec9c3019
rodionovmax <rodionovmax90test@gmail.com>
2023-08-20 11:31:52 -0400

    182

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,4 @@ import { Component } from '@angular/core';
   `]
 })
 export class AppComponent {
-  title = 'new-project';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
